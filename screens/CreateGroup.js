f209399055679fe749a5c19e098f3c14b62c8fbe
Pupun2542 <ppupun2542@gmail.com>
2022-03-10 18:38:24 +0700

    cropper

--- screens/CreateGroup.js
+++ screens/CreateGroup.js
@@ -1,4 +1,4 @@
-import { StyleSheet, View, Text } from "react-native";
+import { StyleSheet, View, Text, ScrollView } from "react-native";
 import React, { useEffect, useState } from "react";
 import { Formik, Field } from "formik";
 import * as Yup from "yup";
@@ -13,31 +13,58 @@ import {
   Box,
   VStack,
   Center,
+  AspectRatio,
+  Image
 } from "native-base";
 import DateTimePicker from "@react-native-community/datetimepicker";
+// import ImagePicker from 'react-native-image-crop-picker';
+import ImageCropPicker from "react-native-image-crop-picker";
 
 const validateSchema = Yup.object().shape({
   Tag: Yup.string()
     .required("ใส่แท็คคอมมูด้วย")
-    .max(4, "แท็คจะต้องมีไม่เกิน 4 ตัวอักษร")
-    .uppercase("พิมพ์ใหญ่ทั้งหมด"),
+    .max(4, "แท็คจะต้องมีไม่เกิน 4 ตัวอักษร"),
   // RunDate: Yup.number().moreThan(0, ""),
   Name: Yup.string().required("ใส่ชื่อคอมมูด้วย"),
   Description: Yup.string().required("ใส่คำอธิบายด้วย"),
 });
 
 const CreateGroup = ({ navigation }) => {
-  // const [date, setDate] = useState(new Date(1598051730000));
+  const [date, setDate] = useState(new Date(1598051730000));
+  const [show, setShow] = useState(false);
+  const [seldate, setSeldate] = useState("");
+  const [banner, setBanner] = useState("https://firebasestorage.googleapis.com/v0/b/comuthor-dev.appspot.com/o/resource%2Fimageplaceholder.png?alt=media&token=b051fff3-c143-4e92-ab5a-7929e3b8edca")
   // const [loading, setLoading] = useState(true);
 
-  // useEffect(() => {
-  //   const d = Date.now();
-  //   setDate(d);
-  //   setLoading(false);
-  // });
+  const handleUpload = ()=>{
+    // ImagePicker.openPicker({
+    //   width: 300,
+    //   height: 400,
+    //   cropping: true
+    // }).then(image => {
+    //   setBanner(image);
+    // });
+    ImageCropPicker.openPicker({
+      width: 300,
+      height: 400,
+      cropping: true
+    }).then(image => {
+        setBanner(image);
+      });
+  }
 
   return (
-    <Container>
+    <Box alignContent="center">
+      <Button backgroundColor={'white'} onPress={handleUpload}>
+      <AspectRatio w="100%" ratio={16 / 9}>
+        <Image
+          source={{
+            uri: banner,
+          }}
+        />
+      </AspectRatio>
+      </Button>
+
       {/* {!loading && ( */}
       <Formik
         //ค่าเริ่มต้นของข้อมูลโดยกำหนดให้ตรงกัน backend
@@ -50,7 +77,7 @@ const CreateGroup = ({ navigation }) => {
           ContactLink: "",
           DocLink: "",
         }}
-        validate={validateSchema}
+        validationSchema={validateSchema}
         // onSubmit={async (values, { setSubmitting }) => {
         //   // same shape as initial values
         //   // alert(JSON.stringify(values));
@@ -82,100 +109,13 @@ const CreateGroup = ({ navigation }) => {
           handleSubmit,
           isSubmitting,
         }) => (
-          // <FormControl>
-          //   {/* กำหนดให้มีเส้นสีแดงถ้าผู้ใช้ไม่กรอกข้อมูลชื่อ */}
-          //   <Box error={errors.Tag && touched.Tag ? true : false}>
-          //     <FormControl.Label>Hashtag#</FormControl.Label>
-          //     <Input
-          //       value={values.Tag}
-          //       onChangeText={handleChange("Tag")}
-          //       onBlur={handleBlur("Tag")}
-          //     />
-          //     {errors.Tag && touched.Tag && <Icon name="close-circle" />}
-          //   </Box>
-          //   <Box
-          //     fixedLabel
-          //     error={errors.Name && touched.Name ? true : false}
-          //   >
-          //     <FormControl.Label>Name</FormControl.Label>
-          //     <Input
-          //       value={values.Name}
-          //       onChangeText={handleChange("Name")}
-          //       onBlur={handleBlur("Name")}
-          //     />
-          //     {errors.Name && touched.Name && <Icon name="close-circle" />}
-          //   </Box>
-          //   {errors.Name && touched.Name && (
-          //     <FormControl.HelperText>
-          //       <FormControl.Label style={{ color: "red" }}>
-          //         {errors.Name}
-          //       </FormControl.Label>
-          //     </FormControl.HelperText>
-          //   )}
-          //   <Box
-          //     fixedLabel
-          //     error={errors.Description && touched.Description ? true : false}
-          //   >
-          //     <FormControl.Label>Description</FormControl.Label>
-          //     <TextArea
-          //       value={values.Description}
-          //       onChangeText={handleChange("Description")}
-          //       onBlur={handleBlur("Description")}
-          //     />
-          //     {errors.Description && touched.Description && (
-          //       <Icon name="close-circle" />
-          //     )}
-          //   </Box>
-          //   <Box fixedLabel>
-          //     <FormControl.Label>Run Date</FormControl.Label>
-          //     {/* <DateTimePicker
-          //       value={date}
-          //       mode={"date"}
-          //       display="default"
-          //       onChange={(e, v) => setDate(v)}
-          //     /> */}
-          //   </Box>
-          //   <Box fixedLabel>
-          //     <FormControl.Label>Contact Link</FormControl.Label>
-          //     <Input
-          //       value={values.ContactLink}
-          //       onChangeText={handleChange("ContactLink")}
-          //       onBlur={handleBlur("ContactLink")}
-          //     />
-          //   </Box>
-          //   <Box fixedLabel>
-          //     <Label>Doc Link</Label>
-          //     <Input
-          //       value={values.DocLink}
-          //       onChangeText={handleChange("DocLink")}
-          //       onBlur={handleBlur("DocLink")}
-          //     />
-          //   </Box>
-          //   <Box fixedLabel>
-          //     <Label>Group Link</Label>
-          //     <Input
-          //       value={values.SMlink}
-          //       onChangeText={handleChange("SMlink")}
-          //       onBlur={handleBlur("SMlink")}
-          //     />
-          //   </Box>
-
-          //   <Button
-          //     block
-          //     large
-          //     style={{ marginTop: 30, backgroundColor: "#E9AB17" }}
-          //     onPress={handleSubmit}
-          //     disabled={isSubmitting}
-          //   >
-          //     <Text
-          //       style={{ color: "white", fontSize: 15, fontWeight: "bold" }}
-          //     >
-          //       Submit
-          //     </Text>
-          //   </Button>
-          // </FormControl>
-          <VStack width="80%" space={4} alignItem="center">
-            <Center>
+          <ScrollView>
+            <VStack
+              width="90%"
+              space={4}
+              justifyContent="center"
+              marginLeft={5}
+            >
               <FormControl isRequired isInvalid={"Tag" in errors}>
                 <FormControl.Label>Hashtag#</FormControl.Label>
                 <Input
@@ -187,12 +127,100 @@ const CreateGroup = ({ navigation }) => {
                   {errors.Tag}
                 </FormControl.ErrorMessage>
               </FormControl>
-            </Center>
-          </VStack>
+              <FormControl isRequired isInvalid={"Name" in errors}>
+                <FormControl.Label>Name</FormControl.Label>
+                <Input
+                  value={values.Name}
+                  onChangeText={handleChange("Name")}
+                  onBlur={handleBlur("Name")}
+                />
+                <FormControl.ErrorMessage>
+                  {errors.Name}
+                </FormControl.ErrorMessage>
+              </FormControl>
+              <FormControl isRequired>
+                <FormControl.Label>Description</FormControl.Label>
+                <TextArea
+                  value={values.Description}
+                  onChangeText={handleChange("Description")}
+                  onBlur={handleBlur("Description")}
+                />
+              </FormControl>
+              <FormControl>
+                <FormControl.Label>Run Date</FormControl.Label>
+                {show && (
+                  <DateTimePicker
+                    value={date}
+                    mode="date"
+                    display="calendar"
+                    onChange={(event, selectedDate) => {
+                      const currentDate = selectedDate || date;
+                      setShow(Platform.OS === "ios");
+                      setDate(currentDate);
+                      const d =
+                        date.getDate() +
+                        "/" +
+                        date.getMonth() +
+                        "/" +
+                        date.getFullYear();
+                      setSeldate(d);
+                      values.RunDate = d;
+                    }}
+                  />
+                )}
+                <Box
+                  // value={values.RunDate}
+                  onTouchStart={() => setShow(true)}
+                  // isDisabled={true}
+                >
+                  {/*  */}
+                  {seldate ? <Text>{seldate}</Text> : <Text>เลือกเวลา</Text>}
+                  {/* {values.RunDate? <Text>{values.RunDate}</Text> : <Text>เลือกเวลา</Text>} */}
+                </Box>
+              </FormControl>
+              <FormControl>
+                <FormControl.Label>Doc Link</FormControl.Label>
+                <Input
+                  value={values.DocLink}
+                  onChangeText={handleChange("DocLink")}
+                  onBlur={handleBlur("DocLink")}
+                />
+              </FormControl>
+              <FormControl>
+                <FormControl.Label>Group Link</FormControl.Label>
+                <Input
+                  value={values.SMlink}
+                  onChangeText={handleChange("SMlink")}
+                  onBlur={handleBlur("SMlink")}
+                />
+              </FormControl>
+              <FormControl>
+                <FormControl.Label>Contact Link</FormControl.Label>
+                <Input
+                  value={values.ContactLink}
+                  onChangeText={handleChange("ContactLink")}
+                  onBlur={handleBlur("ContactLink")}
+                />
+              </FormControl>
+              <Button
+                block
+                large
+                style={{ marginTop: 30, backgroundColor: "#E9AB17" }}
+                onPress={handleSubmit}
+                disabled={isSubmitting}
+              >
+                <Text
+                  style={{ color: "white", fontSize: 15, fontWeight: "bold" }}
+                >
+                  Submit
+                </Text>
+              </Button>
+            </VStack>
+          </ScrollView>
         )}
       </Formik>
       {/* )} */}
-    </Container>
+    </Box>
   );
 };
 
